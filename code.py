6ed73e54677247cc8324c7ddd0bceab241149008
Swapnil Jha <swapniljha001@gmail.com>
2020-10-01 00:09:17 +0530

    Update code.py
    Fixed the sum function definition.

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 # This function adds two numbers
-
+def add(x, y):
+    return x + y
 
 # This function subtracts two numbers
 def subtract(x, y):
@@ -12,7 +13,6 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
     return x / y
-
 
 print("Select operation.")
 print("1.Add")
@@ -41,5 +41,6 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
         break
+        
     else:
         print("Invalid Input")
